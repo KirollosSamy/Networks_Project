e7c13c31ecd59d78b9da9340168be5da7b2eb939
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-12-29 13:41:14 +0200

    na2eshna w kolo tamam <3

--- src/coordinator.cc
+++ src/coordinator.cc
@@ -7,7 +7,8 @@ void Coordinator::initialize()
 {
     std::ifstream file("../input/coordinator.txt");
 
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         std::cerr << "Error opening the coordinator file." << std::endl;
     }
 
@@ -16,9 +17,9 @@ void Coordinator::initialize()
     file >> starting_node >> starting_time;
 
     // send the initial send_frame msg to the sender at the required starting time
-    cMessage* start_msg = new cMessage();
+    cMessage *start_msg = new cMessage();
     start_msg->setKind((short int)MsgType::SEND_FRAME);
     sendDelayed(start_msg, starting_time, "out", starting_node);
 }
 
-void Coordinator::handleMessage(cMessage* msg) {}
+void Coordinator::handleMessage(cMessage *msg) {}
